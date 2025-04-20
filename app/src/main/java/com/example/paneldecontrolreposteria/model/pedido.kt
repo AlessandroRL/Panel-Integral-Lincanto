d68ffaee9dcd3587ef2c4f37795cd54f3abe9eac
AlessandroRL <alessandrorl@ufps.edu.co>
2025-04-20 18:51:16 -0500

    Agregacion y pulimiento eliminacion

--- app/src/main/java/com/example/paneldecontrolreposteria/model/pedido.kt
+++ app/src/main/java/com/example/paneldecontrolreposteria/model/pedido.kt
@@ -5,7 +5,7 @@ import java.util.Date
 data class Pedido(
     var id: String = "",
     val cliente: String = "",
-    val fecha: Date = Date(), // Fecha del pedido
+    val fecha: Date = Date(),
     val productos: List<String> = listOf(),
     val cantidad: Int = 1,
     var estado: String = "Pendiente"
